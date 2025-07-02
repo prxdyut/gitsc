a04470953a76f1731be8f92ffa1546465f2b18d1
Pradyut Das <daspradyut516@gmail.com>
2025-07-02 14:39:27 +0530

    chore: improve Python package metadata and GitHub language detection

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/prxdyut/gitsc",
+    project_urls={
+        "Bug Tracker": "https://github.com/prxdyut/gitsc/issues",
+        "Documentation": "https://github.com/prxdyut/gitsc#readme",
+        "Source Code": "https://github.com/prxdyut/gitsc",
+    },
     packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -19,6 +24,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -26,8 +32,12 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Version Control",
         "Topic :: Software Development :: Version Control :: Git",
+        "Natural Language :: English",
     ],
+    keywords="git, commit, semantic, conventional-commits, ai, llm, groq",
     python_requires=">=3.6",
     install_requires=["groq", "python-dotenv"],
     entry_points={
